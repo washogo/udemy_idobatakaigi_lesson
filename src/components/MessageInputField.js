62017b0f5bdbfffaf959724704a1f9ef37109f31
washogo <aiz4338150@gmail.com>
2022-02-28 13:58:22 +0900

    create message field component

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Grid from "@mui/material/Grid";
 import { makeStyles } from "@mui/styles";
+
+import { gravatarPath } from "../gravatar"
+import MessageField from "./MessageField";
 
 const useStyles = makeStyles({
   root: {
@@ -10,15 +13,20 @@ const useStyles = makeStyles({
   },
 });
 
-const MessageInputField = () => {
+const MessageInputField = ({ name }) => {
+  const [text, setText] = useState();
   const classes = useStyles();
+  const avatarPath = gravatarPath(name)
+
   return (
     <div className={classes.root}>
       <Grid container>
         <Grid xs={1}>
-          <Avatar />
+          <Avatar src={avatarPath} />
         </Grid>
-        <Grid xs={10}>入力</Grid>
+        <Grid xs={10}>
+          <MessageField name={name} setText={setText} text={text} />
+        </Grid>
         <Grid xs={1}>ボタン</Grid>
       </Grid>
     </div>
