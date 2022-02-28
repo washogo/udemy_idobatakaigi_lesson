abbec3ca70ffefee1033f6d9e04d4bbaf93d8beb
washogo <aiz4338150@gmail.com>
2022-02-28 17:42:49 +0900

    fetch messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
 
+import { messagesRef } from "../firebase";
+import { useState } from "react";
+import { useEffect } from "react";
+
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
@@ -8,7 +12,26 @@ const useStyles = makeStyles({
 });
 
 const MessageList = () => {
+  const [messages, setMessages] = useState([]);
   const classes = useStyles();
+
+  useEffect(() => {
+    messagesRef
+    .orderByKey()
+    .limitToLast(3)
+    .on("value", (snapshot) => {
+      const messages = snapshot.val();
+      console.log(messages);
+      if (messages === null) return;
+      const entries = Object.entries(messages);
+      const newMessages = entries.map((entry) => {
+        const [key, nameAndText] = entry;
+        return { key, ...nameAndText };
+      });
+      setMessages(newMessages);
+    });
+  }, [])
+
   return <div className={classes.root}>MessageList</div>;
 };
 
